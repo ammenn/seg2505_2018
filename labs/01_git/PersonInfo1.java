0d3bc248af0eec2adee1f89b3b49fcb26f3a431e
ammenn <43187256+ammenn@users.noreply.github.com>
2018-09-11 17:12:09 -0400

    Update PersonInfo1.java

--- labs/01_git/PersonInfo1.java
+++ labs/01_git/PersonInfo1.java
@@ -1,8 +1,8 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
+    public static String firstName = "amen";
+    public static String lastName = "abshir";
+    public static int studentNo = 300035421;
     public static String description = "Likes to take pictures";
 
     public static void displayFr() {
